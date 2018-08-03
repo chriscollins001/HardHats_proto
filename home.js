9f2e6cc1f6e061d0c9354ecf4d50469196cd4d73
chriscollins001 <chriscollins001@gmail.com>
2018-08-02 21:29:45 -0700

    Search

--- home.js
+++ home.js
@@ -1,10 +1,27 @@
 function ToggleSearch() {
   if ($('.search-control').is(':visible')) {
     $('.search-control, .sort-control').hide();
-    $('btn-search').removeClass('active');
+    $('.btn-search').removeClass('active');
   }
   else {
     $('.search-control, .sort-control').show();
-    $('btn-add').addClass('active');
+    $('.btn-search').addClass('active');
+  }
+}
+
+function UpdateSort() {
+
+}
+
+function SearchDocuments() {
+  var value = $('.input-search').val();
+  if (value != '') {
+    $('.row-document').hide();
+
+    var visibleDocs = $('.row-document').filter(function() {
+        return $(this).attr('data-name').toLowerCase().indexOf(value) > -1 || $(this).attr('data-author').toLowerCase().indexOf(value) > -1 || $(this).attr('data-description').toLowerCase().indexOf(value) > -1;
+    });
+
+    $(visibleDocs).show();
   }
 }
