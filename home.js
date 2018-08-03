06c851b9911b70b4594afed2c4205528bef3f240
chriscollins001 <chriscollins001@gmail.com>
2018-08-02 21:54:52 -0700

    Sort

--- home.js
+++ home.js
@@ -10,7 +10,39 @@ function ToggleSearch() {
 }
 
 function UpdateSort() {
+  var inputSort = $('.input-sort').val();
+  var inputSortDirection = $('.input-sort-direction').val();
 
+  if (inputSort == 'a' && inputSortDirection == 'a') {
+    $('.row-document-container .row-document').sort(function(a,b) {
+         return $(a).data('author') > $(b).data('author');
+    }).appendTo('.row-document-container');
+  }
+  else if (inputSort == 'a' && inputSortDirection == 'd') {
+    $('.row-document-container .row-document').sort(function(a,b) {
+         return $(a).data('author') < $(b).data('author');
+    }).appendTo('.row-document-container');
+  }
+  if (inputSort == 'n' && inputSortDirection == 'a') {
+    $('.row-document-container .row-document').sort(function(a,b) {
+         return $(a).data('name') > $(b).data('name');
+    }).appendTo('.row-document-container');
+  }
+  else if (inputSort == 'n' && inputSortDirection == 'd') {
+    $('.row-document-container .row-document').sort(function(a,b) {
+         return $(a).data('name') < $(b).data('name');
+    }).appendTo('.row-document-container');
+  }
+  else if (inputSort == 'd' && inputSortDirection == 'a') {
+    $('.row-document-container .row-document').sort(function(a,b) {
+         return new Date($(a).data('date')) > new Date($(b).data('date'));
+    }).appendTo('.row-document-container');
+  }
+  else if (inputSort == 'd' && inputSortDirection == 'd') {
+    $('.row-document-container .row-document').sort(function(a,b) {
+         return new Date($(a).data('date')) < new Date($(b).data('date'));
+    }).appendTo('.row-document-container');
+  }
 }
 
 function SearchDocuments() {
